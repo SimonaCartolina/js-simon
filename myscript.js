6eea60f9fc92aae6834ab8eeb7eee8037a88e4c9
Simona Cartolina <simonacartolina@hotmail.com>
2023-05-25 17:10:15 +0200

    fixed missing time

--- myscript.js
+++ myscript.js
@@ -7,15 +7,37 @@ setTimeout(function(){
 }, secondsToShow);
 
 const countdown=setInterval(function(){ 
-    console.log(secondsToShow);
+    console.log(missingTime);
 
-    secondsToShow--;}, 
+    missingTime--;}, 
     1000);
 
-let divContainer=document.querySelector('div.container');
+    /*new Date(0, 0, 0, 19, 0, 0, 0);
+    console.log(Date);
+
+/*let divContainer=document.querySelector('div.container');
 pElement=document.createElement('p');
 pElement.innerHTML= 
 `<p>
 ${'countdown: '+ secondsToShow} 
 </p>`;
+divContainer.appendChild(pElement);*/
+
+/*let divContainer=document.querySelector('div.container');
+pElement=document.createElement('p');
+pElement.innerHTML= 
+`<p>
+${'countdown: '+ Date} 
+</p>`;
+divContainer.appendChild(pElement);*/
+
+let oggi = Date.now("2023-05-25");
+
+const domani = new Date("2023-05-25");
+
+let missingTime= (oggi - domani);
+
+let divContainer=document.querySelector('div.container');
+pElement=document.createElement('p');
+pElement.innerHTML= missingTime;
 divContainer.appendChild(pElement);
